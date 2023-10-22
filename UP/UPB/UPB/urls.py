db46c45f6c2a3f824bcb658c2e32952dd3a528a2
sekongo14 <sekongoismael0@gmail.com>
2023-10-22 13:13:01 +0000

    ok

--- UP/UPB/UPB/urls.py
+++ UP/UPB/UPB/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls, name='admin:index'),
+    # path('admin/', admin.site.urls, name='admin:index'),
     path('siteupb/', include('siteupb.urls')),
     path('Ecount/', include('Ecount.urls')),
 ]
